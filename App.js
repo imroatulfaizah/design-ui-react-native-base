7e0e52795783cdbc4e9e2d1fd61d5ead232ac8fe
faizah.alfaza <faizah.alfaza@gmail.com>
2020-04-16 20:18:44 -0400

    done

--- App.js
+++ App.js
@@ -7,10 +7,13 @@
  */
 
 import React from 'react';
-import {StyleSheet, View} from "react-native";
-import {Form, Item, Input, Text, Body, CheckBox, Button, ListItem, Left, Right, Radio} from 'native-base';
+import {StyleSheet, View, Dimensions} from "react-native";
+import {Form, Item, Input, Text, Body, CheckBox, RadioEdit, RadioNodeList, Button, ListItem, Left, Right, Radio, Grid, Col, Row} from 'native-base';
 import DatePicker from 'react-native-datepicker';
-import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
+// import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
+
+const SCREEN_HEIGHT = Dimensions.get('window').height
+const SCREEN_WIDTH = Dimensions.get('window').width
 
   function App(){
     var radio_props = [
@@ -25,27 +28,60 @@ import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-
 
           <View style={styles.formArea}>
             <Text style={[styles.textContainer, styles.datadiri]}>Data Diri</Text>
+            <View style={{height: SCREEN_HEIGHT * 0.005, width: SCREEN_WIDTH * 0.02, backgroundColor: '#1DDCAF', paddingLeft: 40, width: 50, marginLeft: 30,
+          height: 3}}/>
+
             <Form style={styles.mainForm}>
 
               <Text style={[styles.label, styles.datadiri]}>Nama Lengkap</Text>
-              <Item regular style={[styles.formItems], styles.rounded}>
+              <Item regular style={[styles.formItems, styles.rounded, styles.field]}>
                 <Input placeholder='Nama Lengkap'/>
               </Item>
 
               <Text style={[styles.label, styles.datadiri]}>Tanggal Lahir</Text>
-              <DatePicker placeholder="Tanggal Lahir" style={styles.tanggal}></DatePicker>
-              
+              <DatePicker style={[styles.tanggal, styles.field]} customStyles={{dateInput: {
+            borderColor:'transparent',alignItems:'flex-start', borderWidth: 0.5, fontSize: 12
+          }}} placeholder="Tanggal Lahir"/>
               <Text style={[styles.label, styles.datadiri]}>Alamat</Text>
-              <Item regular style={[styles.formItems], styles.rounded}>
-                <Input placeholder='Alamat'/>
+              <Item regular style={[styles.formItems, styles.rounded, styles.field]}>
+              <Input placeholder='Alamat'/>
               </Item>
 
               <Text style={[styles.label, styles.datadiri]}>Jenis Kelamin</Text>
-              <RadioForm style={styles.tanggal}
-              radio_props={radio_props}
-              initial={0}
-              formHorizontal={true}      
-            />
+              
+            <Grid style={[styles.radiobut]}>
+                <Row>
+                  <Col>
+            <ListItem selected={false} >
+            <Left style={{flex:1}}>
+            <Radio
+                color={"#f0ad4e"}
+                selectedColor={"#5cb85c"}
+                selected={false}                
+              />
+            </Left>
+            <Body style={{flex:5}}>
+            <Text>Laki-laki</Text>
+            </Body>
+          </ListItem>
+                  </Col>
+                  <Col>
+                  <ListItem selected={true}>
+            <Left style={{flex:1}}>
+            <Radio
+                color={"#f0ad4e"}
+                selectedColor={"#5cb85c"}
+                selected={true}
+                
+              />
+            </Left>
+            <Body style={{flex:5}}>
+            <Text>Perempuan</Text>
+            </Body>
+          </ListItem>
+                  </Col>
+                </Row>
+            </Grid>
             </Form>
           </View>
           <View style={styles.Button}>
@@ -70,6 +106,20 @@ import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-
       paddingRight: 12.7,
       paddingLeft: 12.7,
       height: 200,
+    },
+    field: {
+      borderTopStartRadius: 10,
+      borderBottomEndRadius: 10,
+      borderBottomLeftRadius: 10,
+      borderBottomRightRadius: 10,
+      borderBottomStartRadius: 10,
+      borderTopEndRadius: 10,
+      borderColor:'#000',
+      borderWidth: 0.5,
+    },
+    radiobut: {
+      paddingTop: 10,
+      paddingLeft: 10,
     },
     middle: {
       width: '100%',
@@ -96,10 +146,10 @@ import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-
       color: '#FCFDFF',
       fontFamily: "Cochin",
       fontSize: 15,
-     
+      paddingLeft: 30,
       position: 'relative',
       top: '5%',
-      alignSelf: 'center',
+      
       
     },
     formArea: {
@@ -116,7 +166,6 @@ import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-
       borderBottomWidth: 2,
       borderTopWidth: 2,
       borderColor: '#E8E8E8',
-  
     },
     datadiri: {
       top: 0,
@@ -125,15 +174,23 @@ import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-
       marginRight: 220,
       textDecorationColor: '#1DDCAF',
       fontWeight: 'bold',
+      
     },
     nama: {
       top: 0,
       color: '#2D3057',
     },
     tanggal: {
-      paddingLeft: 30,
+      paddingLeft: 7,
       paddingRight:0,
-      marginTop: 15,
+      marginTop: 5,
+      width: '84%',
+      marginLeft: 30,
+      height: 50,
+      paddingTop: 5,
+      borderColor: 'transparent',
+      fontSize: 10,
+      fontWeight:'bold',
       
     },
     formItems: {
@@ -148,6 +205,7 @@ import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-
       borderWidth: 30,
       marginLeft: 30,
       marginRight: 30,
+      borderWidth: 50,
     },
     loginAs: {
       paddingLeft: 46.6,
